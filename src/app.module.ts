e10c72c0f10a67fc5a4307a2aa1657f641f757cc
Javier Flores <jffc.dev@gmail.com>
2024-12-13 16:16:20 -0500

    feat: improvements applied to inital approach

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { join } from 'path';
       typePaths: ['./**/*.graphql'],
       definitions: {
         path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
       },
     }),
   ],
--- src/graphql.ts
+++ src/graphql.ts
@@ -8,13 +8,35 @@
 /* tslint:disable */
 /* eslint-disable */
 
-export interface SignUpInput {
-    email: string;
-    password: string;
-    roleIds: string[];
+export enum ProviderPaymentStatus {
+    SUCCEEDED = "SUCCEEDED",
+    PROCESSING = "PROCESSING",
+    CANCELED = "CANCELED",
+    FAILED = "FAILED"
 }
 
-export interface ProductInput {
+export class SignUpInput {
+    email: string;
+    password: string;
+    phoneNumber?: Nullable<string>;
+    address?: Nullable<string>;
+}
+
+export class LoginInput {
+    email: string;
+    password: string;
+}
+
+export class ResetPasswordInput {
+    token: string;
+    newPassword: string;
+}
+
+export class ForgotPasswordInput {
+    email: string;
+}
+
+export class CreateProductInput {
     name: string;
     description?: Nullable<string>;
     price: number;
@@ -22,125 +44,217 @@ export interface ProductInput {
     categoryIds?: Nullable<string[]>;
 }
 
-export interface BuyInput {
+export class UpdateProductInput {
+    id: string;
+    name?: Nullable<string>;
+    description?: Nullable<string>;
+    price?: Nullable<number>;
+    stock?: Nullable<number>;
+    categoryIds?: Nullable<string[]>;
+}
+
+export class BuyInput {
     products: BuyProductInput[];
 }
 
-export interface BuyProductInput {
+export class BuyProductInput {
     productId: string;
     quantity: number;
 }
 
-export interface IQuery {
-    listProducts(page: number, pageSize: number): Nullable<PaginatedProducts> | Promise<Nullable<PaginatedProducts>>;
-    searchProductsByCategory(categoryId: string): Nullable<Nullable<Product>[]> | Promise<Nullable<Nullable<Product>[]>>;
-    myOrders(): Nullable<Nullable<Order>[]> | Promise<Nullable<Nullable<Order>[]>>;
-    showClientOrders(clientId: string): Nullable<Nullable<Order>[]> | Promise<Nullable<Nullable<Order>[]>>;
+export class CreateRoleInput {
+    name: string;
+    description?: Nullable<string>;
 }
 
-export interface IMutation {
-    signUp(input: SignUpInput): Nullable<AuthPayload> | Promise<Nullable<AuthPayload>>;
-    signIn(email: string, password: string): Nullable<AuthPayload> | Promise<Nullable<AuthPayload>>;
-    signOut(): Nullable<boolean> | Promise<Nullable<boolean>>;
-    forgotPassword(email: string): Nullable<boolean> | Promise<Nullable<boolean>>;
-    resetPassword(token: string, newPassword: string): Nullable<boolean> | Promise<Nullable<boolean>>;
-    createProduct(input: ProductInput): Nullable<Product> | Promise<Nullable<Product>>;
-    updateProduct(productId: string, input: ProductInput): Nullable<Product> | Promise<Nullable<Product>>;
-    deleteProduct(productId: string): Nullable<boolean> | Promise<Nullable<boolean>>;
-    disableProduct(productId: string): Nullable<boolean> | Promise<Nullable<boolean>>;
-    buyProducts(input: BuyInput): Nullable<Order> | Promise<Nullable<Order>>;
-    addProductToCart(productId: string): Nullable<Cart> | Promise<Nullable<Cart>>;
-    likeProduct(productId: string): Nullable<boolean> | Promise<Nullable<boolean>>;
+export class UpdateRoleInput {
+    id: string;
+    name?: Nullable<string>;
+    description?: Nullable<string>;
 }
 
-export interface Product {
+export class CreateCategoryInput {
+    name: string;
+    description?: Nullable<string>;
+}
+
+export class UpdateCategoryInput {
+    id: string;
+    name?: Nullable<string>;
+    description?: Nullable<string>;
+}
+
+export class AddProductToCartInput {
+    productId: string;
+    quantity: number;
+}
+
+export abstract class IQuery {
+    abstract listAndSearchProducts(page: number, pageSize: number, categoryId?: Nullable<string>): Nullable<PaginatedProducts> | Promise<Nullable<PaginatedProducts>>;
+
+    abstract myOrders(): Nullable<Nullable<Order>[]> | Promise<Nullable<Nullable<Order>[]>>;
+
+    abstract showClientOrders(clientId: string): Nullable<Nullable<Order>[]> | Promise<Nullable<Nullable<Order>[]>>;
+}
+
+export abstract class IMutation {
+    abstract signUp(input: SignUpInput): Nullable<AuthPayload> | Promise<Nullable<AuthPayload>>;
+
+    abstract signIn(input: LoginInput): Nullable<AuthPayload> | Promise<Nullable<AuthPayload>>;
+
+    abstract signOut(): Nullable<boolean> | Promise<Nullable<boolean>>;
+
+    abstract forgotPassword(input: ForgotPasswordInput): Nullable<boolean> | Promise<Nullable<boolean>>;
+
+    abstract resetPassword(input: ResetPasswordInput): Nullable<boolean> | Promise<Nullable<boolean>>;
+
+    abstract createProduct(input: CreateProductInput): Nullable<Product> | Promise<Nullable<Product>>;
+
+    abstract updateProduct(input: UpdateProductInput): Nullable<Product> | Promise<Nullable<Product>>;
+
+    abstract deleteProduct(id: string): Nullable<boolean> | Promise<Nullable<boolean>>;
+
+    abstract createRole(input: CreateRoleInput): Nullable<Role> | Promise<Nullable<Role>>;
+
+    abstract updateRole(input: UpdateRoleInput): Nullable<Role> | Promise<Nullable<Role>>;
+
+    abstract deleteRole(roleId: string): Nullable<boolean> | Promise<Nullable<boolean>>;
+
+    abstract createCategory(input: CreateCategoryInput): Nullable<Category> | Promise<Nullable<Category>>;
+
+    abstract updateCategory(input: UpdateCategoryInput): Nullable<Category> | Promise<Nullable<Category>>;
+
+    abstract deleteCategory(id: string): Nullable<boolean> | Promise<Nullable<boolean>>;
+
+    abstract buyProducts(input: BuyInput): Nullable<Order> | Promise<Nullable<Order>>;
+
+    abstract addProductToCart(input: AddProductToCartInput): Nullable<CartItem> | Promise<Nullable<CartItem>>;
+
+    abstract likeProduct(productId: string): Nullable<boolean> | Promise<Nullable<boolean>>;
+}
+
+export class Product {
     id: string;
     name: string;
     description?: Nullable<string>;
     stock?: Nullable<number>;
     isActive: boolean;
     price: number;
-    createdAt: string;
-    updatedAt: string;
+    createdAt: Date;
+    updatedAt: Date;
     images?: Nullable<Nullable<string>[]>;
+    createdBy: string;
+    creator: User;
     categories?: Nullable<Nullable<Category>[]>;
 }
 
-export interface PaginatedProducts {
+export class PaginatedProducts {
     products?: Nullable<Nullable<Product>[]>;
     total: number;
     page: number;
     pageSize: number;
 }
 
-export interface Order {
+export class Order {
     id: string;
-    client: User;
-    products?: Nullable<OrderProduct[]>;
+    clientId: string;
     totalAmount: number;
     status: string;
-    createdAt: string;
+    createdAt: Date;
+    products?: Nullable<OrderDetail[]>;
+    client: User;
+    payments: PaymentAttempt[];
 }
 
-export interface OrderProduct {
+export class PaymentAttempt {
+    id: string;
+    orderId: string;
+    providerCustomerId: string;
+    providerPaymentId: string;
+    providerClientSecret: string;
+    providerStatus: ProviderPaymentStatus;
+    providerAmount: number;
+    providerPaymentMethod: string;
+    createdAt?: Nullable<Date>;
+    updatedAt?: Nullable<Date>;
+    order: Order;
+}
+
+export class OrderDetail {
     product: Product;
     quantity: number;
 }
 
-export interface Cart {
+export class CartItem {
+    userId: string;
+    productId: string;
+    quantity: number;
+    createdAt: Date;
+    updatedAt?: Nullable<Date>;
+    product: Product;
     user: User;
-    items?: Nullable<CartItem[]>;
 }
 
-export interface CartItem {
-    product: Product;
-    quantity: number;
-}
-
-export interface AuthPayload {
+export class AuthPayload {
     token: string;
     user: User;
 }
 
-export interface User {
+export class User {
     id: string;
     email: string;
     phoneNumber?: Nullable<string>;
-    username?: Nullable<string>;
     address?: Nullable<string>;
     verificatedAt?: Nullable<string>;
-    createdAt: string;
-    updatedAt: string;
+    createdAt: Date;
+    updatedAt: Date;
     lastLoginAt?: Nullable<string>;
-    likedProducts?: Nullable<Nullable<Product>[]>;
-    roles?: Nullable<Nullable<Role>[]>;
+    verificationToken?: Nullable<string>;
+    verificationExpiresAt?: Nullable<string>;
+    resetPasswordToken?: Nullable<string>;
+    resetPasswordExpiresAt?: Nullable<string>;
+    likedProducts: Nullable<UserProductLike>[];
+    cartProducts: Nullable<CartItem>[];
+    roles: Role[];
+    createdProducts?: Nullable<Nullable<User>[]>;
+    orders: Order[];
 }
 
-export interface Role {
+export class UserProductLike {
+    productId: string;
+    userId: string;
+    createdAt: Date;
+    user: User;
+    product: Product;
+}
+
+export class Role {
     id: string;
     name: string;
     description?: Nullable<string>;
 }
 
-export interface Category {
+export class Category {
     id: string;
     name: string;
     description?: Nullable<string>;
-    createdAt: string;
-    updatedAt: string;
+    createdAt: Date;
+    updatedAt: Date;
+    products: Product[];
 }
 
-export interface ISubscription {
-    stockLowNotification(productId: string): Nullable<StockLowPayload> | Promise<Nullable<StockLowPayload>>;
-    passwordChangeNotification(userId: string): Nullable<PasswordChangePayload> | Promise<Nullable<PasswordChangePayload>>;
+export abstract class ISubscription {
+    abstract stockLowNotification(productId: string): Nullable<StockLowPayload> | Promise<Nullable<StockLowPayload>>;
+
+    abstract passwordChangeNotification(userId: string): Nullable<PasswordChangePayload> | Promise<Nullable<PasswordChangePayload>>;
 }
 
-export interface StockLowPayload {
+export class StockLowPayload {
     product: Product;
     user: User;
 }
 
-export interface PasswordChangePayload {
+export class PasswordChangePayload {
     user: User;
 }
 
